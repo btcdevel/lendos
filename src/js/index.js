2ad21a7e49adec29916883e15cc1ca54de5cd8f7
Сергей Вилков <btc.devel.gmail.com>
2019-07-21 14:43:56 +0300

    Main popup content

--- src/js/index.js
+++ src/js/index.js
@@ -4,8 +4,11 @@ import bootstrap from "bootstrap";
 
 jQuery(function() {
 
-  const $tariffTag    = $('.tariff__tag');
-  const $tariffBlock  = $('.tariff__card-block');
+  const $tariffTag        = $('.tariff__tag');
+  const $tariffBlock      = $('.tariff__card-block');
+  const $tariffCardTitle  = $('.tariff__card-title');
+  const $tariffPopup      = $('.tariff__popup');
+  const $tariffPopupClose = $('.tariff__popup-close');
 
   $tariffTag.on('click', function() {
     $tariffTag.removeClass('tariff__tag_state_active');
@@ -16,10 +19,21 @@ jQuery(function() {
 
     $( '[data-block="' + $( this ).data('tag') + '"]' ).removeClass('tariff__card-block_state_hide');
     $( '[data-block="' + $( this ).data('tag') + '"]' ).addClass('tariff__card-block_state_active');
-    // $navListItem.removeClass('b-nav__li_state-active');
-    // $(this).addClass('b-nav__li_state-active');
-    // $('.b-nav__dropdown-block').removeClass('b-nav__dropdown-block_state-active');
-    // $( '[data-dropdown="' + $( this ).data('nav-item') + '"]' ).addClass('b-nav__dropdown-block_state-active');
+
+    $( '[data-popup="' + $( this ).data('') + '"]' ).addClass('tariff__card-block_state_active');
+  });
+
+  $tariffCardTitle.on('click', function() {
+
+    console.log($( this ).data('link'));
+    $tariffPopup.removeClass('tariff__popup_state_active');
+    $( '[data-popup="' + $( this ).data('link') + '"]' ).addClass('tariff__popup_state_active');
+  });
+
+  $tariffPopupClose.on('click', function() {
+
+    $tariffPopup.removeClass('tariff__popup_state_active');
+
   });
 
 });
