bda0230797e60c84848f98662d7136b59381a084
Сергей Вилков <btc.devel.gmail.com>
2019-07-21 13:36:45 +0300

    Scripts v1

--- src/js/index.js
+++ src/js/index.js
@@ -5,10 +5,17 @@ import bootstrap from "bootstrap";
 jQuery(function() {
 
   const $tariffTag    = $('.tariff__tag');
+  const $tariffBlock  = $('.tariff__card-block');
 
   $tariffTag.on('click', function() {
     $tariffTag.removeClass('tariff__tag_state_active');
     $( this ).addClass('tariff__tag_state_active');
+
+    $tariffBlock.addClass('tariff__card-block_state_hide');
+    $tariffBlock.removeClass('tariff__card-block_state_active');
+
+    $( '[data-block="' + $( this ).data('tag') + '"]' ).removeClass('tariff__card-block_state_hide');
+    $( '[data-block="' + $( this ).data('tag') + '"]' ).addClass('tariff__card-block_state_active');
     // $navListItem.removeClass('b-nav__li_state-active');
     // $(this).addClass('b-nav__li_state-active');
     // $('.b-nav__dropdown-block').removeClass('b-nav__dropdown-block_state-active');
