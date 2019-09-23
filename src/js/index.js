20062f2fc1b52bbe39a1372267b3e950e5a045aa
Сергей Вилков <btc.devel.gmail.com>
2019-09-23 12:31:03 +0300

    Minor

--- src/js/index.js
+++ src/js/index.js
@@ -4,35 +4,35 @@ import bootstrap from "bootstrap";
 
 jQuery(function() {
 
-  const $tariffTag        = $('.tariff__tag');
-  const $tariffBlock      = $('.tariff__card-block');
-  const $tariffCardTitle  = $('.tariff__card-title');
-  const $tariffPopup      = $('.tariff__popup');
-  const $tariffPopupClose = $('.tariff__popup-close');
+  const $tariffTag        = $('.b-tariff__tag');
+  const $tariffBlock      = $('.b-tariff__card-block');
+  const $tariffCardTitle  = $('.b-tariff__card-title');
+  const $tariffPopup      = $('.b-tariff__popup');
+  const $tariffPopupClose = $('.b-tariff__popup-close');
 
   $tariffTag.on('click', function() {
-    $tariffTag.removeClass('tariff__tag_state_active');
-    $( this ).addClass('tariff__tag_state_active');
+    $tariffTag.removeClass('b-tariff__tag_state_active');
+    $( this ).addClass('b-tariff__tag_state_active');
 
-    $tariffBlock.addClass('tariff__card-block_state_hide');
-    $tariffBlock.removeClass('tariff__card-block_state_active');
+    $tariffBlock.addClass('b-tariff__card-block_state_hide');
+    $tariffBlock.removeClass('b-tariff__card-block_state_active');
 
-    $( '[data-block="' + $( this ).data('tag') + '"]' ).removeClass('tariff__card-block_state_hide');
-    $( '[data-block="' + $( this ).data('tag') + '"]' ).addClass('tariff__card-block_state_active');
+    $( '[data-block="' + $( this ).data('tag') + '"]' ).removeClass('b-tariff__card-block_state_hide');
+    $( '[data-block="' + $( this ).data('tag') + '"]' ).addClass('b-tariff__card-block_state_active');
 
-    $( '[data-popup="' + $( this ).data('') + '"]' ).addClass('tariff__card-block_state_active');
+    $( '[data-popup="' + $( this ).data('') + '"]' ).addClass('b-tariff__card-block_state_active');
   });
 
   $tariffCardTitle.on('click', function() {
 
     console.log($( this ).data('link'));
-    $tariffPopup.removeClass('tariff__popup_state_active');
-    $( '[data-popup="' + $( this ).data('link') + '"]' ).addClass('tariff__popup_state_active');
+    $tariffPopup.removeClass('b-tariff__popup_state_active');
+    $( '[data-popup="' + $( this ).data('link') + '"]' ).addClass('b-tariff__popup_state_active');
   });
 
   $tariffPopupClose.on('click', function() {
 
-    $tariffPopup.removeClass('tariff__popup_state_active');
+    $tariffPopup.removeClass('b-tariff__popup_state_active');
 
   });
 
